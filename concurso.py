187fdc0e90a6c901c270f54d209fbc5d86f31291
DiegoCoira <114064760+DiegoCoira@users.noreply.github.com>
2022-10-13 11:56:26 +0200

    Add files via upload

--- concurso.py
+++ concurso.py
@@ -1,14 +1,48 @@
-respuesta= ''
+
 print("Cual de estos personajes es un centinela")
 print("a) Sage.")
 print("b) Raze.")
 print("c) Jett.")
 respuesta = input("Elige una(a,b,c): ")
+
 if respuesta == 'a':
-    print("Correcto")
+    resultado = 10
 elif respuesta == 'b':
-    print("Fallo")
+    resultado = -5
 elif respuesta == 'c':
-    print("Incorrecto")
+    resultado = -5
 else:
-    print("Formato no soportado")
+    resultado = -5
+
+print("Cual de estos personajes es un duelista")
+print("a) Skye.")
+print("b) Reyna.")
+print("c) Killjoy.")
+respuesta = input("Elige una(a,b,c): ")
+
+if respuesta == 'a':
+    resultado = resultado -5
+elif respuesta == 'b':
+    resultado = resultado +10
+elif respuesta == 'c':
+    resultado = resultado - 5
+else:
+    resultado = resultado -5
+
+print("Cual de estos personajes es un iniciador")
+print("a) Kayo.")
+print("b) Viper.")
+print("c) Neon.")
+respuesta = input("Elige una(a,b,c): ")
+
+if respuesta == 'a':
+    resultado = resultado +10
+elif respuesta == 'b':
+    resultado = resultado + -5
+elif respuesta == 'c':
+    resultado = resultado + -5
+else:
+    resultado = resultado + -5
+
+print(" ")
+print("tu nota es un " + str(resultado) + "/30")

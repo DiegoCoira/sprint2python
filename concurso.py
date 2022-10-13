e0aa0df964cd0803787a05620283267be039850a
DiegoCoira <114064760+DiegoCoira@users.noreply.github.com>
2022-10-13 11:59:20 +0200

    Add files via upload
    Añadidos los comentarios

--- concurso.py
+++ concurso.py
@@ -1,10 +1,10 @@
-
+#Primera pregunta
 print("Cual de estos personajes es un centinela")
 print("a) Sage.")
 print("b) Raze.")
 print("c) Jett.")
 respuesta = input("Elige una(a,b,c): ")
-
+#Comprobacion respuestas
 if respuesta == 'a':
     resultado = 10
 elif respuesta == 'b':
@@ -14,12 +14,13 @@ elif respuesta == 'c':
 else:
     resultado = -5
 
+#Segunda pregunta
 print("Cual de estos personajes es un duelista")
 print("a) Skye.")
 print("b) Reyna.")
 print("c) Killjoy.")
 respuesta = input("Elige una(a,b,c): ")
-
+#Comprobación respuestas
 if respuesta == 'a':
     resultado = resultado -5
 elif respuesta == 'b':
@@ -28,13 +29,13 @@ elif respuesta == 'c':
     resultado = resultado - 5
 else:
     resultado = resultado -5
-
+#Tercera pregunta
 print("Cual de estos personajes es un iniciador")
 print("a) Kayo.")
 print("b) Viper.")
 print("c) Neon.")
 respuesta = input("Elige una(a,b,c): ")
-
+#Comprobación respuestas
 if respuesta == 'a':
     resultado = resultado +10
 elif respuesta == 'b':
@@ -44,5 +45,7 @@ elif respuesta == 'c':
 else:
     resultado = resultado + -5
 
+#Print para crear espacio entre la ultima pregunta y la respuesta
 print(" ")
+#Resultado Final
 print("tu nota es un " + str(resultado) + "/30")

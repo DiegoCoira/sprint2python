ce798b4f8b2913bf64256d48d070ebb3d04d88f7
DiegoCoira <114064760+DiegoCoira@users.noreply.github.com>
2022-10-17 12:37:40 +0200

    Add files via upload
    Añadido ejercicio 5

--- principal.py
+++ principal.py
@@ -1,6 +1,16 @@
 from factorial import func_factorial
 
-n = int(input("Dame un número: "))
-solucion=func_factorial(n)
-solucion2=func_factorial2(n)
+print("a)Factorial con recursividad")
+print("b)Factorial sin recursividad")
+print("")
+opcion= str(input("Elige una opción(a,b):"))
+print("")
+if opcion == "a":
+    n = int(input("Dame un número: "))
+    solucion=func_factorial(n)
+    
+if opcion == "b":
+    n = int(input("Dame un número: "))
+    solucion2=func_factorial2(n)
+    
 print("El numero factorial de " + str(n) + " es: "+ str(solucion) + ".")

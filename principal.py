7ddad5ed0f50d4c8160d6384f91c232667dfdcc2
DiegoCoira <114064760+DiegoCoira@users.noreply.github.com>
2022-10-17 12:51:37 +0200

    Correcón + Ejercicio 6
    Correción de ejercicios anteriores + Añadido ejercicio 6

--- principal.py
+++ principal.py
@@ -1,4 +1,7 @@
 from factorial import func_factorial
+import time
+from datetime import datetime
+from factorial2 import func_factorial2
 
 print("a)Factorial con recursividad")
 print("b)Factorial sin recursividad")
@@ -7,10 +10,17 @@ opcion= str(input("Elige una opción(a,b):"))
 print("")
 if opcion == "a":
     n = int(input("Dame un número: "))
+    start_time = time.time()
     solucion=func_factorial(n)
+    end_time = time.time()
     
 if opcion == "b":
     n = int(input("Dame un número: "))
-    solucion2=func_factorial2(n)
+    start_time = time.time()
+    end_time = time.time()
+    solucion=func_factorial2(n)
+    
+    
     
 print("El numero factorial de " + str(n) + " es: "+ str(solucion) + ".")
+print("La ejecución tardo " + str(end_time-start_time) + " en ejecutarse.")
